2889fbe9dc107de239d77065cc81436968934ed0
MaoGi <vancii1201@163.com>
2024-12-08 00:45:25 +0800

    测试探索

--- dev_tools/assets_test.py
+++ dev_tools/assets_test.py
@@ -34,10 +34,10 @@ def detect_ocr(file: str, target: RuleOcr):
 
 
 # 图片文件路径 可以是相对路径
-IMAGE_FILE = r"C:\Users\Ryland\Desktop\363385164-41fa341c-3efc-43f8-91b5-0cdd065ed8cf.png"
+IMAGE_FILE = r"C:\Users\vanci\Desktop\1.png"
 if __name__ == '__main__':
-    from tasks.KekkaiUtilize.assets import KekkaiUtilizeAssets
-    targe = KekkaiUtilizeAssets.I_SHI_CARD
+    from tasks.Exploration.assets import ExplorationAssets
+    targe = ExplorationAssets.O_E_EXPLORATION_LEVEL_NUMBER
     print(detect_image(IMAGE_FILE, targe))
 
     # ocr demo
